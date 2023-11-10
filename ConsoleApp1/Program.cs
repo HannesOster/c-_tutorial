24fd9cc999e4d265c46105b4bfb40a67f4610550
Johannes Osterkamp <johannes.osterkamp@gmx.net>
2023-11-10 05:05:07 +0100

    add perator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,11 +12,13 @@ namespace HalloWelt
         {
             byte age = 29;
             double bankAccount = 2381.323445;
-            byte dayOfWeek = 1;
+            int dayOfWeek = 1;
+            byte daysGone = 4;
+            dayOfWeek = dayOfWeek + daysGone;
             string name = "Johannes";
             bool married = false;
-
-
+            Console.WriteLine(dayOfWeek);
+            
         }
     }
 }
